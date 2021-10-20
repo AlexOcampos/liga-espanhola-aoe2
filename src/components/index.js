f76ea7518a027a1ed815a1738f958721053332af
Alex Ocampos <alexocamposveiga@gmail.com>
2021-10-20 14:51:17 +0200

    Merge "filters" branch

--- src/components/index.js
+++ src/components/index.js
@@ -4,9 +4,16 @@ import Notification from "./Notification";
 import Hero from "./Hero";
 import Sidebar from "./Sidebar";
 import FeaturedGame from "./FeaturedGame";
-
-export { Navbar, Footer, Notification, Hero, Sidebar, FeaturedGame };
 import Filters from "./Filters";
 import Player from "./Player";
 
-export { Navbar, Footer, Notification, Hero, Sidebar, Filters, Player };
+export {
+  Navbar,
+  Footer,
+  Notification,
+  Hero,
+  Sidebar,
+  Filters,
+  Player,
+  FeaturedGame,
+};
